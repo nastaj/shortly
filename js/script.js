13f5557ba35ca2408894c5cff069c718eed3d5b4
nastaj <G00424689@atu.ie>
2023-08-06 15:46:32 +0200

    added popups & bugfixes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 "use strict";
 
-// DOM Elements
+const header = document.querySelector(".section-head");
+const mobileNav = document.querySelector(".nav-mobile");
+const mobileNavBtn = document.querySelector(".nav__mobile");
 const shortener = document.querySelector(".shortener");
 const linksList = document.querySelector(".links-list");
 const form = document.querySelector(".shortener__form");
 const input = document.querySelector(".shortener__input");
 const btn = document.querySelector(".shortener__btn");
 
-// App
 class App {
   #links = [];
   #link;
@@ -15,33 +16,57 @@ class App {
 
   constructor() {
     this._getLocalStorage();
+
+    mobileNavBtn.addEventListener("click", this._openNav.bind(this));
+    mobileNav.addEventListener("click", this._hideNav.bind(this));
     form.addEventListener("submit", this._submitForm.bind(this));
   }
 
+  _openNav() {
+    mobileNav.classList.toggle("nav-mobile--hidden");
+  }
+
+  _hideNav(e) {
+    const target = e.target;
+    console.log(target);
+    if (target.classList.contains("nav-mobile__item")) {
+      mobileNav.classList.add("nav-mobile--hidden");
+    }
+  }
+
   async _submitForm(e) {
-    e.preventDefault();
-    this.#link = input.value;
+    try {
+      e.preventDefault();
+      this.#link = input.value;
 
-    await this._shortenLink(this.#link);
+      await this._shortenLink(this.#link);
 
-    const linkObject = {
-      original: this.#link,
-      shortened: this.#linkShortened,
-    };
+      const linkObject = {
+        original: this.#link,
+        shortened: this.#linkShortened,
+      };
+      this.#links.push(linkObject);
 
-    this.#links.push(linkObject);
-    this._setLocalStorage();
-    this._renderLink(this.#links);
-    console.log(this.#links);
+      this._setLocalStorage();
+      this._renderLink(this.#links);
+      this._addHandlers();
+      this._togglePopup("success", "Link successfully shortened!");
+    } catch (err) {
+      this._togglePopup("error", "There was a problem. Try another link!");
+    }
   }
 
   async _shortenLink(link) {
-    const response = await fetch(
-      `https://api.shrtco.de/v2/shorten?url=${link}`
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${link}`
+      );
+      const data = await response.json();
 
-    this.#linkShortened = data.result.full_short_link;
+      this.#linkShortened = data.result.full_short_link;
+    } catch (err) {
+      throw err;
+    }
   }
 
   _renderLink() {
@@ -61,13 +86,75 @@ class App {
   _generateMarkup(newLink, oldLink = "") {
     return `
     <div class="container links">
-        <a href="#" class="links__before">${oldLink ? oldLink : this.#link}</a>
+        <a href="${
+          oldLink ? oldLink : this.#link
+        }" class="links__before" target="_blank">${
+      oldLink ? oldLink : this.#link
+    }</a>
         <div class="links__after-box">
-          <a href="#" class="links__after">${newLink}</a>
+          <a href="${newLink}" class="links__after" target="_blank">${newLink}</a>
           <button class="btn-cta links__btn-copy">Copy</button>
         </div>
     </div>
     `;
+  }
+
+  _addHandlers() {
+    const copyBtns = document.querySelectorAll(".links__btn-copy");
+    copyBtns.forEach((btn) =>
+      btn.addEventListener("click", this._copyToClipboard)
+    );
+  }
+
+  async _copyToClipboard(e) {
+    const copyBtns = document.querySelectorAll(".links__btn-copy");
+    const btn = e.target;
+    const link = btn
+      .closest(".links")
+      .querySelector(".links__after").textContent;
+
+    copyBtns.forEach((btn) => {
+      btn.classList.remove("links__btn-copy--active");
+      btn.textContent = "Copy";
+    });
+
+    btn.classList.add("links__btn-copy--active");
+    btn.textContent = "Copied!";
+
+    try {
+      await navigator.clipboard.writeText(link);
+    } catch (err) {
+      this._togglePopup(
+        "error",
+        `Could not copy the link to clipboard: ${err}`
+      );
+    }
+  }
+
+  _togglePopup(state = "error", msg) {
+    const markup = `
+    <div class="popup ${
+      state === "success" ? "success" : "error"
+    }-popup new-popup">
+      <p>${msg}</p>
+    </div>
+    `;
+    header.insertAdjacentHTML("beforebegin", markup);
+
+    const popups = document.querySelectorAll(".popup");
+    popups.forEach((popup) => this._hidePopup(popup));
+  }
+
+  async _hidePopup(popup) {
+    await this._pause(3);
+    popup.classList.add("hide-popup");
+    await this._pause(0.2);
+    popup.style.visibility = "hidden";
+    popup.remove();
+  }
+
+  _pause(seconds) {
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
   }
 
   _setLocalStorage() {
@@ -80,6 +167,7 @@ class App {
     if (dataLinks) this.#links = dataLinks;
 
     this._renderAllLinks();
+    this._addHandlers();
   }
 }
 const app = new App();
